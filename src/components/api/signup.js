e3b9a4c0be2d30104400c19d914f1cd73f7e7d49
brygulley25 <brygulley25@gmail.com>
2023-02-06 15:54:15 -0800

    make it better

--- src/components/api/signup.js
+++ src/components/api/signup.js
@@ -40,6 +40,8 @@ app.post('/api/signup', (req, res) => {
   });
 });
 
+let port = process.env.PORT || 3000 
+
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log(`Server running on port ${port}`);
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import App from './App';
 
+
 import { createRoot } from 'react-dom/client';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
